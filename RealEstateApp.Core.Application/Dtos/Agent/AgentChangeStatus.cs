5a8e2de638b0c3826283849db037d0504a46c518
jjjbaezz <87380605+jjjbaezz@users.noreply.github.com>
2025-04-22 16:10:24 -0400

    Agent dtos

---
+++ RealEstateApp.Core.Application/Dtos/Agent/AgentChangeStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace RealEstateApp.Core.Application.Dtos.Agent
+{
+    internal class AgentChangeStatus
+    {
+        [SwaggerParameter(Description = "Indica si el agente esta activo (true) o inactivo (false)")]
+        [Required(ErrorMessage = "IsActive is required")]
+        public bool IsActive { get; set; }
+    }
+}
---
+++ RealEstateApp.Infrastructure.Persistence/Repositories/AgentRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealEstateApp.Infrastructure.Persistence.Contexts;
+using RealEstateApp.Infrastructure.Persistence.Migrations;
+
+namespace RealEstateApp.Infrastructure.Persistence.Repositories
+{
+    public class AgentRepository 
+    {
+        private readonly ApplicationContext _context;
+
+    }
+}
